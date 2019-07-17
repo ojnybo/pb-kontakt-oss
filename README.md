cf5cc58e819f2eac20337556a6d3e5610094aacd
Morten Jansrud <morten@founder.no>
2019-07-17 11:07:13 +0200

    Legg til circleci pipeline, codeowners, license, dockerfil og nais setup

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# tilbakemeldinger
-Klage, anke og ros
+# Tilbakemeldinger
+
+For klage, anke og ros
+
+## Komme i gang
+
+Hent repoet fra github
+
+```
+git clone https://github.com/navikt/tilbakemeldinger.git
+```
+
+Installer nødvendige pakker:
+
+```
+npm install
+```
+
+Start applikasjonen lokalt:
+
+```
+npm start
+```
+
+
+## Deployering
+
+Applikasjonen bygges automatisk til dev / https://www-q0.nav.no/person/tilbakemeldinger ved merge til master eller ved manuell godkjenning i [CircleCI](https://circleci.com/gh/navikt/workflows/tilbakemeldinger). <br><br>
+For å lansere applikasjonen til produksjon / https://www.nav.no/person/tilbakemeldinger, benytt [npm version](https://docs.npmjs.com/cli/version) til å oppdatere package.json og lage samsvarende Git-tag. Eks:
+
+```
+npm version patch -m "Din melding"
+```
+
+Push deretter den nye versjonen til GitHub og merge til master.
+
+```
+git push && git push --tags
+```
+
+Godkjenn produksjonssettingen i [CircleCI](https://circleci.com/gh/navikt/workflows/tilbakemeldinger).
+
+## Logging
+
+Feil ved API-kall blir logget via frontendlogger og vises i Kibana<br>
+[https://logs.adeo.no](https://logs.adeo.no/app/kibana#/discover/ad01c200-4af4-11e9-a5a6-7fddb220bd0c)
+
+## Henvendelser
+
+Spørsmål knyttet til koden eller prosjektet kan rettes mot https://github.com/orgs/navikt/teams/personbruker
+
+## For NAV-ansatte
+
+Interne henvendelser kan sendes via Slack i kanalen #team-personbruker.
---
+++ server/server.js
@@ -0,0 +1,75 @@
+require("dotenv").config();
+
+const express = require("express");
+const path = require("path");
+const mustacheExpress = require("mustache-express");
+const getDecorator = require("./dekorator");
+const server = express();
+
+server.set("views", `${__dirname}/../build`);
+server.set("view engine", "mustache");
+server.engine("html", mustacheExpress());
+
+// parse application/json
+server.use(express.json());
+
+server.use((req, res, next) => {
+  res.removeHeader("X-Powered-By");
+  next();
+});
+
+const renderApp = decoratorFragments =>
+  new Promise((resolve, reject) =>
+    server.render("index.html", decoratorFragments, (err, html) => {
+      if (err) reject(err);
+      resolve(html);
+    })
+  );
+
+const startServer = html => {
+  server.use(
+    "/person/tilbakemeldinger/static/js",
+    express.static(path.resolve(`${__dirname}/..`, "build/static/js"))
+  );
+
+  server.use(
+    "/person/tilbakemeldinger/static/media",
+    express.static(path.resolve(`${__dirname}/..`, "build/static/media"))
+  );
+
+  server.use(
+    "/person/tilbakemeldinger/index.css",
+    express.static(path.resolve(`${__dirname}/..`, "build/index.css"))
+  );
+
+  server.use(
+    "/person/tilbakemeldinger/manifest.json",
+    express.static(path.resolve(`${__dirname}/..`, "build/manifest.json"))
+  );
+
+  server.use(
+    "/person/tilbakemeldinger/favicon.ico",
+    express.static(path.resolve(`${__dirname}/..`, "build/favicon.ico"))
+  );
+
+  server.get("/person/tilbakemeldinger/internal/alive|ready", (req, res) =>
+    res.sendStatus(200)
+  );
+
+  server.use("/person/tilbakemeldinger", (req, res) => {
+    res.send(html);
+  });
+
+  const port = process.env.PORT || 8080;
+  server.listen(port, () => console.log(`App listening on port: ${port}`));
+};
+
+const logError = (errorMessage, details) => console.log(errorMessage, details);
+
+getDecorator()
+  .then(renderApp, error => logError("Failed to get decorator", error))
+  .then(startServer, error => logError("Failed to render app", error));
+
+process.on("SIGTERM", () =>
+  setTimeout(() => console.log("Har sovet i 30 sekunder"), 30000)
+);
