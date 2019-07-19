082c02110cf1e91aef8909db659cfa295816ac84
Morten Jansrud <morten@founder.no>
2019-07-19 13:08:14 +0200

    Test av notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import logo from './assets/logo.svg';
+import React from "react";
+import logo from "./assets/logo.svg";
 
 const App: React.FC = () => {
   return (
@@ -20,6 +20,6 @@ const App: React.FC = () => {
       </header>
     </div>
   );
-}
+};
 
 export default App;
