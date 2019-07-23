61bee1229f544c40127e8b7abb704d2abb550557
Morten Jansrud <morten@founder.no>
2019-07-23 10:28:46 +0200

    Implementer veilder

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Frontpage from "./pages/frontpage/Frontpage";
+import Header from "./components/header/Header";
 
 const App: React.FC = () => {
   const baseUrl = "/person/tilbakemeldinger";
   return (
-    <div className="pagecontent">
-      <Router>
+    <Router>
+      <Header />
+      <div className="pagecontent">
         <Route path={`(|${baseUrl})`} component={Frontpage} />
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 };
 
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { withRouter, RouteComponentProps } from "react-router";
+import { Element } from "nav-frontend-typografi";
+import Veileder from "nav-frontend-veileder";
+import VeilederIcon from "../../assets/Veileder.svg";
+
+const ColorBar = (props: RouteComponentProps) => {
+  console.log(props);
+  return props.location.pathname === "/" ? (
+    <div className="header header__frontpage">
+      <div className="header__icon">
+        <Veileder
+          tekst="Hei! Vi vil gjerne høre om opplevelsen din!"
+          posisjon="topp"
+        >
+          <img src={VeilederIcon} alt="Veileder" />
+        </Veileder>
+      </div>
+    </div>
+  ) : (
+    <div className="header">
+      <Element>Tilbakemelding på service</Element>
+    </div>
+  );
+};
+
+export default withRouter(ColorBar);
