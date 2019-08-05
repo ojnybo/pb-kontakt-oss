fea3a21bee18624f97678cbd94d0a99b2087d89d
Morten Jansrud <morten@founder.no>
2019-08-05 13:47:59 +0200

    Reintroduser container

--- src/components/input-fields/RadioPanelGruppe.tsx
+++ src/components/input-fields/RadioPanelGruppe.tsx
@@ -11,13 +11,15 @@ const RPG = (props: Props) => {
   const { onChange, submitted, feilmelding, ...newProps } = props;
 
   return (
-    <RadioPanelGruppe
-      onChange={(event: React.SyntheticEvent<EventTarget>, value: string) =>
-        onChange(value)
-      }
-      feil={submitted && !props.checked ? { feilmelding } : undefined}
-      {...newProps}
-    />
+    <div className="rpg__rad">
+      <RadioPanelGruppe
+        onChange={(event: React.SyntheticEvent<EventTarget>, value: string) =>
+          onChange(value)
+        }
+        feil={submitted && !props.checked ? { feilmelding } : undefined}
+        {...newProps}
+      />
+    </div>
   );
 };
 
