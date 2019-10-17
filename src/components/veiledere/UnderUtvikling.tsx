9f72200c0a9f3cf9626490b7b50daf898f7cc9f4
Morten Jansrud <morten.jansrud@nav.no>
2019-10-17 18:23:55 +0200

    Legg til under utvikling

---
+++ src/components/veiledere/UnderUtvikling.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Veilederpanel from "nav-frontend-veilederpanel";
+import VeilederIcon from "assets/Veileder.svg";
+
+const UnderUtvikling = () => (
+  <Veilederpanel svg={<img src={VeilederIcon} />} fargetema={"advarsel"}>
+    <b>Denne siden er under utvikling.</b>
+    <br />
+    Kom gjerne tilbake senere!
+  </Veilederpanel>
+);
+
+export default UnderUtvikling;
