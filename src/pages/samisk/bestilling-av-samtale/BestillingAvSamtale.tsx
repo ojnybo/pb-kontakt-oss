23c94159e3e86b4abc2db07a7bac965fd50b12a8
Morten Jansrud <morten.jansrud@nav.no>
2019-10-24 15:10:59 +0200

    Fjern lenke

--- src/pages/samisk/bestilling-av-samtale/BestillingAvSamtale.tsx
+++ src/pages/samisk/bestilling-av-samtale/BestillingAvSamtale.tsx
@@ -81,6 +81,9 @@ const BAS = (props: RouteComponentProps) => {
 
   return (
     <div className="pagecontent">
+      <div className="tilbake__lenke lenke">
+        <a href={"https://www.nav.no/se/Samegiella"}>Tilbake</a>
+      </div>
       <div className="bestilling-av-samtale__header">
         <div className="bestilling-av-samtale__tittel">
           <Sidetittel>
@@ -88,135 +91,129 @@ const BAS = (props: RouteComponentProps) => {
           </Sidetittel>
         </div>
       </div>
-      <Box>
-        <FormValidation
-          onSubmit={send}
-          config={formConfig}
-          initialValues={initialValues}
-        >
-          {({ errors, fields, submitted, setField, isValid }) => {
-            return (
-              <>
-                <div className="bestilling-av-samtale__ingress">
-                  <AlertStripeInfo>
-                    <Normaltekst>
-                      Diŋgo dás davvisámegilli bálvalusa mas vástiduvvo dutnje
-                      sámegillii buot NAV – bálvalusain ja oajuin. Mii veahkehit
-                      gávdnat mo du áššiin manná, ja veahkehit du dovdat
-                      rivttiid ja geatnegasvuođaid mat leat álbmotoadjolága
-                      njuolggadusain. Don gávnnat dieđuid iežat áššis
-                      neahttabálvalusas nav.no Ditt NAV. Don sáhtát iskat mii
-                      dutnje lea máksojuvvon dás:
-                    </Normaltekst>
-                    <br />
-                    <Normaltekst>
-                      Don sáhtat ain riŋget NAV-bálvalussii 55 55 33 33 ja
-                      dáhtot ahte davvisámegielat bagadalli riŋge dutnje. Muite
-                      addit riegadan- ja persunnummara ja maid telefunnummara
-                      masa davvisámegielat galga riŋget.
-                    </Normaltekst>
-                  </AlertStripeInfo>
-                </div>
-                <InputNavn
-                  bredde={"M"}
-                  label={"Ovdanamma"}
-                  value={fields.fornavn}
-                  error={errors.fornavn}
-                  onChange={v => setField({ fornavn: v })}
-                  submitted={submitted}
-                />
-                <InputNavn
-                  bredde={"M"}
-                  label={"Goargu"}
-                  value={fields.etternavn}
-                  error={errors.etternavn}
-                  onChange={v => setField({ etternavn: v })}
-                  submitted={submitted}
-                />
-                <InputTelefon
-                  bredde={"M"}
-                  label={"Telefovdna*"}
-                  value={fields.telefonnummer}
-                  error={errors.telefonnummer}
-                  onChange={v => setField({ telefonnummer: v })}
-                  submitted={submitted}
-                />
-                <div className="bestilling-av-samtale__tidsrom">
-                  <legend className="skjema__legend">
-                    Goas heive duinna váldit oktavuođa?
-                  </legend>
-                  <SkjemaGruppe
-                    feil={
-                      submitted && errors.tidsrom
-                        ? { feilmelding: errors.tidsrom }
-                        : undefined
-                    }
+      <FormValidation
+        onSubmit={send}
+        config={formConfig}
+        initialValues={initialValues}
+      >
+        {({ errors, fields, submitted, setField, isValid }) => {
+          return (
+            <Box>
+              <div className="bestilling-av-samtale__ingress">
+                <AlertStripeInfo>
+                  <Normaltekst>
+                    Diŋgo dás davvisámegilli bálvalusa mas vástiduvvo dutnje
+                    sámegillii buot NAV – bálvalusain ja oajuin. Mii veahkehit
+                    gávdnat mo du áššiin manná, ja veahkehit du dovdat rivttiid
+                    ja geatnegasvuođaid mat leat álbmotoadjolága njuolggadusain.
+                    Don gávnnat dieđuid iežat áššis neahttabálvalusas nav.no
+                    Ditt NAV. Don sáhtát iskat mii dutnje lea máksojuvvon dás:
+                  </Normaltekst>
+                  <br />
+                  <Normaltekst>
+                    Don sáhtat ain riŋget NAV-bálvalussii 55 55 33 33 ja dáhtot
+                    ahte davvisámegielat bagadalli riŋge dutnje. Muite addit
+                    riegadan- ja persunnummara ja maid telefunnummara masa
+                    davvisámegielat galga riŋget.
+                  </Normaltekst>
+                </AlertStripeInfo>
+              </div>
+              <InputNavn
+                bredde={"M"}
+                label={"Ovdanamma"}
+                value={fields.fornavn}
+                error={errors.fornavn}
+                onChange={v => setField({ fornavn: v })}
+                submitted={submitted}
+              />
+              <InputNavn
+                bredde={"M"}
+                label={"Goargu"}
+                value={fields.etternavn}
+                error={errors.etternavn}
+                onChange={v => setField({ etternavn: v })}
+                submitted={submitted}
+              />
+              <InputTelefon
+                bredde={"M"}
+                label={"Telefovdna*"}
+                value={fields.telefonnummer}
+                error={errors.telefonnummer}
+                onChange={v => setField({ telefonnummer: v })}
+                submitted={submitted}
+              />
+              <div className="bestilling-av-samtale__tidsrom">
+                <legend className="skjema__legend">
+                  Goas heive duinna váldit oktavuođa?
+                </legend>
+                <SkjemaGruppe
+                  feil={
+                    submitted && errors.tidsrom
+                      ? { feilmelding: errors.tidsrom }
+                      : undefined
+                  }
+                >
+                  <Checkbox
+                    label={"08.00-10.00"}
+                    value={"FORMIDDAG"}
+                    onChange={e => {
+                      const value = e.target.value;
+                      setField({
+                        tidsrom: {
+                          ...fields.tidsrom,
+                          [value]: !fields.tidsrom[value]
+                        }
+                      });
+                    }}
+                  />
+                  <Checkbox
+                    label={"13.30-15.30"}
+                    value={"ETTERMIDDAG"}
+                    onChange={e => {
+                      const value = e.target.value;
+                      setField({
+                        tidsrom: {
+                          ...fields.tidsrom,
+                          [value]: !fields.tidsrom[value]
+                        }
+                      });
+                    }}
+                  />
+                </SkjemaGruppe>
+              </div>
+              <div>
+                {error && (
+                  <AlertStripeFeil>Oi! Noe gikk galt: {error}</AlertStripeFeil>
+                )}
+              </div>
+              <div className="bestilling-av-samtale__knapper">
+                <div className="bestilling-av-samtale__knapp">
+                  <Knapp
+                    type={"standard"}
+                    htmlType={"submit"}
+                    disabled={loading || (submitted && !isValid)}
                   >
-                    <Checkbox
-                      label={"08.00-10.00"}
-                      value={"FORMIDDAG"}
-                      onChange={e => {
-                        const value = e.target.value;
-                        setField({
-                          tidsrom: {
-                            ...fields.tidsrom,
-                            [value]: !fields.tidsrom[value]
-                          }
-                        });
-                      }}
-                    />
-                    <Checkbox
-                      label={"13.30-15.30"}
-                      value={"ETTERMIDDAG"}
-                      onChange={e => {
-                        const value = e.target.value;
-                        setField({
-                          tidsrom: {
-                            ...fields.tidsrom,
-                            [value]: !fields.tidsrom[value]
-                          }
-                        });
-                      }}
-                    />
-                  </SkjemaGruppe>
+                    {loading ? (
+                      <NavFrontendSpinner type={"S"} />
+                    ) : (
+                      "Sádde jearaldaga"
+                    )}
+                  </Knapp>
                 </div>
-                <div>
-                  {error && (
-                    <AlertStripeFeil>
-                      Oi! Noe gikk galt: {error}
-                    </AlertStripeFeil>
-                  )}
+                <div className="bestilling-av-samtale__knapp">
+                  <Flatknapp
+                    onClick={() => {
+                      window.location.href = "https://www.nav.no/se/Samegiella";
+                    }}
+                  >
+                    Gå tilbake
+                  </Flatknapp>
                 </div>
-                <div className="bestilling-av-samtale__knapper">
-                  <div className="bestilling-av-samtale__knapp">
-                    <Knapp
-                      type={"standard"}
-                      htmlType={"submit"}
-                      disabled={loading || (submitted && !isValid)}
-                    >
-                      {loading ? (
-                        <NavFrontendSpinner type={"S"} />
-                      ) : (
-                        "Sádde jearaldaga"
-                      )}
-                    </Knapp>
-                  </div>
-                  <div className="bestilling-av-samtale__knapp">
-                    <Flatknapp
-                      onClick={() => {
-                        window.location.href =
-                          "https://www.nav.no/se/Samegiella";
-                      }}
-                    >
-                      Gå tilbake
-                    </Flatknapp>
-                  </div>
-                </div>
-              </>
-            );
-          }}
-        </FormValidation>
-      </Box>
+              </div>
+            </Box>
+          );
+        }}
+      </FormValidation>
     </div>
   );
 };
