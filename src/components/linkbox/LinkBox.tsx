027ed91a09f63fc387426f2f39bdab289db25441
Morten Jansrud <morten.jansrud@nav.no>
2019-10-25 10:27:38 +0200

    Bruke linkbokser over panel?

--- src/components/linkbox/LinkBox.tsx
+++ src/components/linkbox/LinkBox.tsx
@@ -17,9 +17,11 @@ export interface Props {
 const Box = (props: Props) => {
   return (
     <>
-      <div className="linkbox__icon-container icon__container">
-        <Icon backgroundImage={props.icon} />
-      </div>
+      {props.icon && (
+        <div className="linkbox__icon-container icon__container">
+          <Icon backgroundImage={props.icon} />
+        </div>
+      )}
       <div className="linkbox__content">
         <div className="linkbox__seksjon">
           <div className="linkbox__tittel">
--- src/pages/tilbakemeldinger/Tilbakemeldinger.tsx
+++ src/pages/tilbakemeldinger/Tilbakemeldinger.tsx
@@ -1,11 +1,10 @@
 import React from "react";
-import { Normaltekst, Undertittel } from "nav-frontend-typografi";
 import { lenker } from "./TilbakemeldingerLenker";
-import { LenkepanelBase } from "nav-frontend-lenkepanel";
-import { Link } from "react-router-dom";
 import Tilbake from "../../components/tilbake/Tilbake";
 import { urls } from "../../Config";
 import Header from "../../components/header/Header";
+import LinkBox from "../../components/linkbox/LinkBox";
+import Box from "../../components/box/Box";
 
 const Tilbakemeldinger = () => {
   document.title = "Tilbakemeldinger - www.nav.no";
@@ -13,46 +12,20 @@ const Tilbakemeldinger = () => {
     <>
       <div className="pagecontent">
         <Tilbake to={urls.forside} />
-        <div className={"tilbakemeldinger__header"}>
-          <Header title={"Tilbakemeldinger til NAV"} />
-        </div>
-        <div className="tilbakemeldinger">
-          <div className="tilbakemeldinger__content">
-            {lenker.map((lenke, key) => (
-              <LenkepanelBase
-                key={key}
-                border={true}
-                className="lenke__panel"
-                href={lenke.lenke}
-                linkCreator={props => {
-                  return lenke.external ? (
-                    <a href={lenke.lenke} className={props.className}>
-                      {props.children}
-                    </a>
-                  ) : (
-                    <Link to={lenke.lenke} className={props.className}>
-                      {props.children}
-                    </Link>
-                  );
-                }}
-              >
-                <div className="lenke__container">
-                  <div className="lenke__tittel">
-                    <Undertittel className="lenkepanel__heading">
-                      {lenke.tittel}
-                    </Undertittel>
-                  </div>
-                  <div className="lenke__beskrivelse">
-                    <Normaltekst>{lenke.beskrivelse}</Normaltekst>
-                  </div>
-                  <div className="lenke">
-                    <Normaltekst>{lenke.lenkeTekst}</Normaltekst>
-                  </div>
-                </div>
-              </LenkepanelBase>
-            ))}
-          </div>
-        </div>
+        <Header title={"Tilbakemeldinger til NAV"} />
+        <Box>
+          {lenker.map(lenke => (
+            <LinkBox
+              key={lenke.tittel}
+              id={lenke.tittel}
+              tittel={lenke.tittel}
+              beskrivelse={lenke.beskrivelse}
+              to={lenke.lenke}
+              lenkeTekst={lenke.lenkeTekst}
+              component={lenke.external ? "a" : "Link"}
+            />
+          ))}
+        </Box>
       </div>
     </>
   );
