167f097ed49597ada091f5897e2ea42d57fc938e
Morten Jansrud <morten.jansrud@nav.no>
2019-10-25 08:52:20 +0200

    Lenkepanel -> bokser

--- src/Environments.ts
+++ src/Environments.ts
@@ -1,40 +1,46 @@
 const Environment = () => {
-  if (process.env.NODE_ENV === "development") {
+  var host = window.location.host;
+  var subdomain = host.split(`.`)[0];
+
+  if (process.env.NODE_ENV === `development`) {
     return {
-      miljo: "LOCAL",
-      baseUrl: "http://localhost:8080",
-      appUrl: "http://localhost:8080/person/kontakt-oss",
-      apiUrl: "http://localhost:8080/person/tilbakemeldinger-api",
-      personInfoApiUrl: "http://localhost:8080/person/personopplysninger-api",
-      tjenesteUrl: "https://tjenester-q0.nav.no",
-      loginUrl: "http://localhost:8080/personbruker-api/local/cookie",
-      logoutUrl: "#",
-      unleashUrl: "#",
+      miljo: `LOCAL`,
+      baseUrl: `https://www.nav.no`,
+      appUrl: `http://localhost:8080/person/kontakt-oss`,
+      apiUrl: `http://localhost:8080/person/tilbakemeldinger-api`,
+      personInfoApiUrl: `http://localhost:8080/person/personopplysninger-api`,
+      tjenesteUrl: `https://tjenester.nav.no`,
+      loginUrl: `http://localhost:8080/personbruker-api/local/cookie`,
+      logoutUrl: `#`,
+      unleashUrl: `#`
     };
   }
-  if (window.location.hostname.indexOf("www-q0") > -1) {
+  if (subdomain !== `www`) {
+    // Preprod - Q0, Q1 etc
+    const env = subdomain.split(`-`)[1];
     return {
-      miljo: "DEV",
-      baseUrl: "https://www-q0.nav.no",
-      appUrl: "https://www-q0.nav.no/person/kontakt-oss",
-      apiUrl: "https://www-q0.nav.no/person/tilbakemeldinger-api",
-      personInfoApiUrl: "https://www-q0.nav.no/person/personopplysninger-api",
-      tjenesteUrl: "https://tjenester-q0.nav.no",
-      loginUrl: "https://loginservice-q.nav.no/login",
-      logoutUrl: "https://loginservice-q.nav.no/slo",
-      unleashUrl: "https://www-q0.nav.no/person/dittnav/api/feature",
+      miljo: `DEV`,
+      baseUrl: `https://www-${env}.nav.no`,
+      appUrl: `https://www-${env}.nav.no/person/kontakt-oss`,
+      apiUrl: `https://www-${env}.nav.no/person/tilbakemeldinger-api`,
+      personInfoApiUrl: `https://www-${env}.nav.no/person/personopplysninger-api`,
+      tjenesteUrl: `https://tjenester-${env}.nav.no`,
+      loginUrl: `https://loginservice-q.nav.no/login`,
+      logoutUrl: `https://loginservice-q.nav.no/slo`,
+      unleashUrl: `https://www-${env}.nav.no/person/dittnav/api/feature`
     };
   }
+
   return {
-    miljo: "PROD",
-    baseUrl: "https://www.nav.no",
-    appUrl: "https://www.nav.no/person/kontakt-oss",
-    apiUrl: "https://www.nav.no/person/tilbakemeldinger-api",
-    personInfoApiUrl: "https://www.nav.no/person/personopplysninger-api",
-    tjenesteUrl: "https://tjenester.nav.no",
-    loginUrl: "https://loginservice.nav.no/login",
-    logoutUrl: "https://loginservice.nav.no/slo",
-    unleashUrl: "https://www.nav.no/person/dittnav/api/feature",
+    miljo: `PROD`,
+    baseUrl: `https://www.nav.no`,
+    appUrl: `https://www.nav.no/person/kontakt-oss`,
+    apiUrl: `https://www.nav.no/person/tilbakemeldinger-api`,
+    personInfoApiUrl: `https://www.nav.no/person/personopplysninger-api`,
+    tjenesteUrl: `https://tjenester.nav.no`,
+    loginUrl: `https://loginservice.nav.no/login`,
+    logoutUrl: `https://loginservice.nav.no/slo`,
+    unleashUrl: `https://www.nav.no/person/dittnav/api/feature`
   };
 };
 
--- src/pages/kontakt-oss-frontpage/sections/lenkepanel/LenkePanel.tsx
+++ src/pages/kontakt-oss-frontpage/sections/lenkepanel/LenkePanel.tsx
@@ -1,27 +1,42 @@
 import React from "react";
 import eksterneLenker from "./LenkePanelData";
-import LinkBox from "./linkbox/LinkBox";
-import PanelBase from "nav-frontend-paneler";
+import Box from "../../../../components/box/Box";
+import { Undertittel } from "nav-frontend-typografi";
 
 const LenkePanel = () => {
   return (
-    <PanelBase className="el__panel">
-      <div className="el__content">
-        {eksterneLenker.map(link => (
-          <LinkBox
-            id={link.id}
-            key={link.id}
-            icon={link.icon}
-            tittel={link.tittel}
-            beskrivelse={link.beskrivelse}
-            lenkeTekst={link.lenkeTekst}
-            to={link.url}
-            component={link.type as "Link" | "a"}
-          />
-        ))}
-      </div>
-    </PanelBase>
+    <>
+      {eksterneLenker.map(link => (
+        <Box>
+          <>
+            <div className={"box__section-title"}>
+              <Undertittel className="box__title">{link.tittel}</Undertittel>
+            </div>
+            <div className={"box__section-description"}>
+              <span dangerouslySetInnerHTML={{ __html: link.beskrivelse }} />
+            </div>
+            {link.url && link.lenkeTekst && (
+              <a className="lenke" href={link.url}>
+                {link.lenkeTekst}
+              </a>
+            )}
+          </>
+        </Box>
+      ))}
+    </>
   );
 };
 
+/*
+  <LinkBox
+    id={link.id}
+    key={link.id}
+    icon={link.icon}
+    tittel={link.tittel}
+    beskrivelse={link.beskrivelse}
+    lenkeTekst={link.lenkeTekst}
+    to={link.url}
+    component={link.type as "Link" | "a"}
+  />
+ */
 export default LenkePanel;
