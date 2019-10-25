2d4914f020e67a435f6eb86d6a0ea0bbceffc4e6
Morten Jansrud <morten.jansrud@nav.no>
2019-10-25 11:26:50 +0200

    Reorder imports

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,9 +1,9 @@
+// Browsers
+import "react-app-polyfill/ie9";
+import "react-app-polyfill/ie11";
+
 // Functions
 // @ts-ignore
 if (!global || !global._babelPolyfill) {
   require("babel-polyfill");
 }
-
-// Browsers
-import "react-app-polyfill/ie9";
-import "react-app-polyfill/ie11";
