7aaea438e079dd88074768c53bb3d2f10c1a83ee
Morten Jansrud <morten.jansrud@nav.no>
2019-10-30 15:59:24 +0100

    Aktiver veiledere igjen i prod

--- src/components/veiledere/UnderUtvikling.tsx
+++ src/components/veiledere/UnderUtvikling.tsx
@@ -4,22 +4,19 @@ import VeilederIcon from "assets/Veileder.svg";
 import Lenke from "nav-frontend-lenker";
 import { urls } from "../../Config";
 
-const utvikling = false;
-
-const UnderUtvikling = () =>
-  utvikling ? (
-    <Veilederpanel
-      svg={<img src={VeilederIcon} alt={"Veileder"} />}
-      fargetema={"advarsel"}
-    >
-      <b>Denne siden er under utvikling.</b>
-      <br />
-      <div className={"faq__lenke"}>
-        <Lenke href={urls.gamleKontaktOss}>
-          Gå til de gamle kontakt-oss sidene
-        </Lenke>
-      </div>
-    </Veilederpanel>
-  ) : null;
+const UnderUtvikling = () => (
+  <Veilederpanel
+    svg={<img src={VeilederIcon} alt={"Veileder"} />}
+    fargetema={"advarsel"}
+  >
+    <b>Denne siden er under utvikling.</b>
+    <br />
+    <div className={"faq__lenke"}>
+      <Lenke href={urls.gamleKontaktOss}>
+        Gå til de gamle kontakt-oss sidene
+      </Lenke>
+    </div>
+  </Veilederpanel>
+);
 
 export default UnderUtvikling;
