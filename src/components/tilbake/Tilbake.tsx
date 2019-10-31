87f77004617825f37ab85778a62e06945a53fbb1
Morten Jansrud <morten.jansrud@nav.no>
2019-10-31 09:08:46 +0100

    Fiks radiobuttons

--- src/components/tilbake/Tilbake.tsx
+++ src/components/tilbake/Tilbake.tsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { FormattedMessage } from "react-intl";
+import { VenstreChevron } from "nav-frontend-chevron";
 
 interface Props {
   to: string;
 }
 
 const Tilbake = (props: Props) => (
-  <div className="tilbake__lenke lenke">
-    <Link to={props.to}>Tilbake</Link>
+  <div className={"tilbake__lenke-container"}>
+    <Link className={"tilbake__lenke lenke"} to={props.to}>
+      <VenstreChevron />
+      <span>
+        <FormattedMessage id={"felter.tilbake"} />
+      </span>
+    </Link>
   </div>
 );
 
