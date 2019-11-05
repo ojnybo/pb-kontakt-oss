5e70d7769b500be1367249988e73a3b7b33d688d
Morten Jansrud <morten.jansrud@nav.no>
2019-11-05 09:50:55 +0100

    Refaktor

--- server/logger.js
+++ server/logger.js
@@ -1,7 +1,11 @@
 const winston = require("winston");
 
 const logger = winston.createLogger({
-  transports: [new winston.transports.Console({ timestamp: true })]
+  transports: [
+    new winston.transports.Console({
+      timestamp: true
+    })
+  ]
 });
 
 module.exports = logger;
