495aed83d479edba72a4fd2093e70edb4ee996f5
Morten Jansrud <morten.jansrud@nav.no>
2019-11-05 09:50:13 +0100

    Legg til timestamp

--- server/logger.js
+++ server/logger.js
@@ -1,7 +1,7 @@
 const winston = require("winston");
 
 const logger = winston.createLogger({
-  transports: [new winston.transports.Console()]
+  transports: [new winston.transports.Console({ timestamp: true })]
 });
 
 module.exports = logger;
