6d8b6bca838ac556b60823b49966cb2329b28c30
anders-nom <anders@nomerstad.com>
2019-11-13 13:22:14 +0100

    Sletter cookie for chat-session på mellomsider som går til gammel chat-løsning.

--- src/pages/chat/sider/ChatSosial.tsx
+++ src/pages/chat/sider/ChatSosial.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import ChatTemaSide from "../ChatTemasideBase";
 import { ChatTema } from "../../../types/chat";
 import FormattedMsgMedParagrafer from "../../../components/intl-msg-med-paragrafer/FormattedMsgMedParagrafer";
+import { deleteCookie } from "../../../utils/cookies";
+import { vars } from "../../../Config";
 
 const ChatSosial = () => {
+  deleteCookie(vars.chatBot.oldChatbotCookieName);
+
   return(
     <ChatTemaSide
       tittelId={"chat.sosialhjelp.tittel"}
