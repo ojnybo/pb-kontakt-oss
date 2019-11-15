9c1c630875132e70401b29feb7ae53ec84796422
Morten Jansrud <morten.jansrud@nav.no>
2019-11-15 15:29:44 +0100

    Grå bakgrunn, endre avstand mellom komponenter

--- src/components/ikonpanel/IkonPanel.tsx
+++ src/components/ikonpanel/IkonPanel.tsx
@@ -15,11 +15,13 @@ const cssPrefix = "ikonpanel";
 const IkonPanel = ({ ikon, tittel, children, className }: Props) => {
   return (
     <Panel className={`${cssPrefix}${className ? ` ${className}` : ""}`}>
-      {ikon && (
-        <div className={`${cssPrefix}__ikon-container`}>
-          <img src={ikon} alt="" className={`${cssPrefix}__ikon`} />
-        </div>
-      )}
+      <div>
+        {ikon && (
+          <div className={`${cssPrefix}__ikon-container`}>
+            <img src={ikon} alt="" className={`${cssPrefix}__ikon`} />
+          </div>
+        )}
+      </div>
       <div className={`${cssPrefix}__innhold`}>
         <Systemtittel className={`${cssPrefix}__innhold-header`}>
           {tittel}
