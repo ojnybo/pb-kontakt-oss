2669718c8205b76698b6d9bcc5d8ecac64bd96fd
Morten Jansrud <morten.jansrud@nav.no>
2019-11-25 15:25:15 +0100

    Fiks byggproblemer

--- src/pages/chat/ChatLenkepanelData.tsx
+++ src/pages/chat/ChatLenkepanelData.tsx
@@ -5,35 +5,41 @@ import { FormattedMessage } from "react-intl";
 
 const chatTemaLenker: Array<LenkepanelData> = [
   {
+    grafanaId: "chat.jobbsoker",
     tittelId: "chat.jobbsoker.tittel",
     ingress: <FormattedMessage id={"chat.jobbsoker.ingressKort"} />,
     url: urls.chat.jobbsoker.temaside
   },
   {
+    grafanaId: "chat.aap",
     tittelId: "chat.aap.tittel",
     ingress: <FormattedMessage id={"chat.aap.ingressKort"} />,
     url: urls.chat.aap.temaside
   },
   {
+    grafanaId: "chat.familie",
     tittelId: "chat.familie.tittel",
     ingress: <FormattedMessage id={"chat.familie.ingressKort"} />,
     url: urls.chat.familie.temaside
   },
   {
+    grafanaId: "chat.sosialhjelp",
     tittelId: "chat.sosialhjelp.tittel",
     ingress: <FormattedMessage id={"chat.sosialhjelp.ingressKort"} />,
     url: urls.chat.sosialhjelp.temaside
   },
   {
+    grafanaId: "chat.okonomi",
     tittelId: "chat.okonomi.tittel",
     ingress: <FormattedMessage id={"chat.okonomi.ingressKort"} />,
     url: urls.chat.okonomi.temaside
   },
   {
+    grafanaId: "chat.eures",
     tittelId: "chat.eures.tittel",
     ingress: <FormattedMessage id={"chat.eures.ingressKort"} />,
     url: urls.chat.eures.temaside
-  },
+  }
 ];
 
 export default chatTemaLenker;
