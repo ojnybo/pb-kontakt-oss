f22285a461770d3d017df3a13b5e300f0b94122b
Morten Jansrud <morten.jansrud@nav.no>
2019-11-25 14:31:43 +0100

    Logg ring-oss

--- src/pages/kontakt-oss-frontpage/sections/RingOss.tsx
+++ src/pages/kontakt-oss-frontpage/sections/RingOss.tsx
@@ -5,13 +5,18 @@ import IkonPanel from "../../../components/ikonpanel/IkonPanel";
 import ikon from "assets/forside-ringoss-ikon.svg";
 import { urls } from "../../../Config";
 import RouterLenke from "../../../components/routerlenke/RouterLenke";
+import { logEvent } from "../../../utils/logger";
 
 const RingOss = () => {
   const tittel = <FormattedMessage id={"kontaktoss.ringoss.tittel"} />;
 
+  const onClick = () => {
+    logEvent({ event: "ring-oss" });
+  };
+
   return (
     <IkonPanel ikon={ikon} tittel={tittel} className="ringoss">
-      <RouterLenke isExternal={true} href={urls.ringOss}>
+      <RouterLenke isExternal={true} href={urls.ringOss} onClick={onClick}>
         <FormattedMessage id={"kontaktoss.ringoss.knapp"} />
       </RouterLenke>
     </IkonPanel>
