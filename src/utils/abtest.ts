c80b5466d37f37e7a0336374bbc39e853a5f82a8
anders-nom <anders@nomerstad.com>
2019-11-26 13:55:53 +0100

    Oppdatert lenker ++

--- src/utils/abtest.ts
+++ src/utils/abtest.ts
@@ -1,6 +1,6 @@
 import Cookies from "js-cookie";
 
-const testSluttTid = new Date("2019-11-27T10:00:00+01:00");
+const testSluttTid = new Date("2019-11-28T00:01:00+01:00");
 const cookieName = "kontakt-ab2";
 const kontrollGruppeVariant = "gammel";
 const testGruppeVariant = "ny";
