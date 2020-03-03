f8f7824ccb4f93948f2d7df22082b43cce15dbff
anders-nom <anders@nomerstad.com>
2020-03-03 09:00:22 +0100

    Endrer tekst på FAQ om koronavirus Fjerner avvikende åpningstid på økonomi-chat

--- src/pages/chat/sider/ChatOkonomi.tsx
+++ src/pages/chat/sider/ChatOkonomi.tsx
@@ -3,7 +3,6 @@ import ChatTemaSideBase from "../ChatTemasideBase";
 import { ChatTema, ChatTemaData } from "../../../types/chat";
 import FormattedMsgMedParagrafer from "../../../components/intl-msg-med-paragrafer/FormattedMsgMedParagrafer";
 import { apningsTider } from "../../../Config";
-import TidsbestemtVisning from "../../../utils/TidsbestemtVisning";
 
 const chatTemaData: ChatTemaData = {
   tittelTekstId: "chat.okonomi.tittel",
@@ -18,7 +17,6 @@ const ChatOkonomi = () => {
       chatTemaData={chatTemaData}
     >
       <>
-        <TidsbestemtVisning til={"23:59 02-03-2020"}><b>{"Chatten er dessverre stengt i dag."}</b></TidsbestemtVisning>
         <FormattedMsgMedParagrafer id={"chat.okonomi.ingress"} />
       </>
     </ChatTemaSideBase>
